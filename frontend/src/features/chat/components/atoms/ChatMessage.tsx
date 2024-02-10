47198b86da85fbbb9dc3605c0c1595e5e6336422
Kenta Noda <noda.5552.k@gmail.com>
2024-02-10 15:55:56 +0900

    🐛 メッセージ間のマージン追加

--- frontend/src/features/chat/components/atoms/ChatMessage.tsx
+++ frontend/src/features/chat/components/atoms/ChatMessage.tsx
@@ -2,7 +2,7 @@ import { ChatMessageProps } from "../../chat.types";
 
 export const ChatMessage = ({ message, time}: ChatMessageProps) => {
   return (
-    <div className="flex items-end space-x-2">
+    <div className="flex items-end space-x-2 mb-[0.8rem]">
       <div className='bg-light-gray rounded-[18px] py-[6px] px-4'>
         <p className='font-normal text-white break-words'>{message}</p>
       </div>
