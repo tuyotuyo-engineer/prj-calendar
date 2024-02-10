f66dd4614461e018769aa024e512ed6d0ce0ad66
Kenta Noda <noda.5552.k@gmail.com>
2024-02-10 10:40:11 +0900

    💄 calendarページ画面構成作成

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -1,9 +1,16 @@
-import { Header } from "@/components/organisms/Header/Header.organism";
+import { Header } from '@/components/organisms/Header/Header.organism';
+import Chat from '@/features/chat/components/Chat';
 
 export default function Calendar() {
   return (
-    <main className="px-12 py-16 bg-red-400">
+    <main className="flex flex-col gap-8 h-screen px-12 py-16 bg-red-400">
       <Header />
+      <div className="flex h-full">
+        <div className="flex flex-col w-[32rem] bg-yellow-300"></div>
+        <div className="flex w-full bg-blue-300"></div>
+        <div className="flex flex-col w-[32rem] bg-green-300"></div>
+      </div>
     </main>
   );
 }
+
