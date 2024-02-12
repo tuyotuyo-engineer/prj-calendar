08048bf85e37312a9c0fb823dcfcd8e884f79223
Kenta Noda <noda.5552.k@gmail.com>
2024-02-12 16:03:11 +0900

    💄 Tasks作成

--- frontend/src/features/calendar/components/Tasks.tsx
+++ frontend/src/features/calendar/components/Tasks.tsx
@@ -1,5 +1,9 @@
-export const Tasks = () => {
+import { TasksProps } from '../types';
+
+export const Tasks = ({ taskTitle }: TasksProps) => {
   return (
-    <div className='min-w-[12rem] h-[10rem] bg-orange-400/75 rounded-3xl'></div>
+    <div className='min-w-[12rem] h-[10rem] bg-orange-400/75 rounded-3xl'>
+      <p className='p-4'>{taskTitle}</p>
+    </div>
   );
 };
