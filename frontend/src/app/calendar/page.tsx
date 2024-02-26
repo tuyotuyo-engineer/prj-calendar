63c901463668aa412c59ebb99ef90abc6bece807
tzptzptzptzptzp <git@tzp.pink>
2024-02-26 22:38:59 +0900

    💄 クラスの指定方法を変更

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -1,24 +1,22 @@
-'use client'
+'use client';
 import { Button } from '@/components/atoms/Button/Button.atom';
 import { Header } from '@/components/organisms/Header/Header.organism';
 import Chat from '@/features/chat/components/Chat';
 
 export default function Calendar() {
   return (
-    <main className='flex flex-col gap-8 h-screen px-12 py-16 bg-red-400'>
+    <main className='flex flex-col gap-16 h-screen px-12 py-16 bg-red-400'>
       <Header />
       <div className='flex h-full'>
         <div className='flex flex-col w-[32rem] bg-yellow-300'></div>
         <div className='flex w-full bg-blue-300'></div>
-        <div className='flex flex-col w-[32rem] bg-green-300 gap-16'>
+        <div className='flex flex-col gap-16 w-[32rem] bg-green-300'>
           <Button
             variant='tagEdit'
-            className='w-full h-[6rem] border-[0.3rem] border-white rounded-[3rem] bg-white/75 shadow-sm'
+            className='section-base'
             onClick={() => console.log('タグフォームのモーダルオープン')}
           >
-            <p className='font-normal text-[2rem] text-custom-black flex justify-center items-center'>
-              スケジュールタグ設定
-            </p>
+            スケジュールタグ設定
           </Button>
           <Chat />
         </div>
