c4bb2c6d5b5e297dd75fba7571acac8ac975e7a5
tzptzptzptzptzp <git@tzp.pink>
2024-03-11 20:52:30 +0900

    🩹 スネークケースエラーを解消

--- frontend/src/components/icons/Arrow/Arrow.icon.tsx
+++ frontend/src/components/icons/Arrow/Arrow.icon.tsx
@@ -1,48 +1,48 @@
-import { IconType } from "../icon.type";
+import { IconType } from '../icon.type';
 
 type ArrowIconType = {
-  direction: "left" | "right";
+  direction: 'left' | 'right';
 } & IconType;
 
 export const ArrowIcon = ({
-  color = "#444",
+  color = '#444',
   direction,
   width = 14,
   height = 17,
 }: ArrowIconType) => {
-  if (direction === "left") {
+  if (direction === 'left') {
     return (
       <svg
         width={width}
         height={height}
-        viewBox="0 0 14 17"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+        viewBox='0 0 14 17'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
       >
         <path
-          d="M13 1L1 8.5L13 16"
+          d='M13 1L1 8.5L13 16'
           stroke={color}
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth='1.5'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
       </svg>
     );
-  } else if (direction === "right") {
+  } else if (direction === 'right') {
     return (
       <svg
         width={width}
         height={height}
-        viewBox="0 0 14 17"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+        viewBox='0 0 14 17'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
       >
         <path
-          d="M1 16L13 8.5L1 1"
+          d='M1 16L13 8.5L1 1'
           stroke={color}
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth='1.5'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
       </svg>
     );
