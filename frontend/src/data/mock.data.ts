97b105763501ef71a37010707b8cfdb28c0fad99
tzptzptzptzptzp <git@tzp.pink>
2024-03-11 20:41:46 +0900

    🤡 モックデータを作成

---
+++ frontend/src/data/mock.data.ts
@@ -0,0 +1,25 @@
+const groupData = {
+  groupName: '',
+  userList: [
+    {
+      name: 'Hiroki Tetuka',
+      selected: true,
+    },
+    {
+      name: 'Kenta Noda',
+      selected: true,
+    },
+    {
+      name: 'Tomoya Negishi',
+      selected: false,
+    },
+    {
+      name: 'Yamada Tarou',
+      selected: true,
+    },
+  ],
+};
+
+export const mockData = {
+  groupData,
+};
