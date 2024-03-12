4568eca493928936eaa5f3a0076f68e37e5fc57e
tzptzptzptzptzp <git@tzp.pink>
2024-03-12 22:52:16 +0900

    ✨ ミニカレンダーを実装

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/atoms/Button/Button.atom';
 import { Header } from '@/components/organisms/Header/Header.organism';
 import { Chat } from '@/features/chat/components/Chat';
 import { Group } from '@/features/group/components/organisms/Group.organism';
+import { MiniCalendar } from '@/features/miniCalendar/components/MiniCalendar'; // Fixed import statement
 import { mockData } from '@/data/mock.data';
 
 export default function Calendar() {
@@ -10,7 +11,8 @@ export default function Calendar() {
     <main className='flex flex-col gap-16 h-screen px-12 py-16 bg-red-400'>
       <Header />
       <div className='flex h-full'>
-        <div className='flex flex-col w-[32rem] bg-yellow-300'>
+        <div className='flex flex-col gap-16 w-[32rem] bg-yellow-300'>
+          <MiniCalendar />
           <Group
             groupName={mockData.groupData.groupName}
             initUserList={mockData.groupData.userList}
