5c3f1ef58113b73bfb9cbf47aeeeef521ba0d21a
tzptzptzptzptzp <git@tzp.pink>
2024-03-12 23:33:34 +0900

    ✨ [今日]ボタンを押した際に状態をリセットする処理を追加

--- frontend/src/components/organisms/Header/Header.organism.tsx
+++ frontend/src/components/organisms/Header/Header.organism.tsx
@@ -5,6 +5,7 @@ import { CurrentDate } from '@/components/atoms/CarrentDate/CurrentDate.atom';
 import { Button } from '@/components/atoms/Button/Button.atom';
 import { ArrowIcon } from '@/components/icons/Arrow/Arrow.icon';
 import { Logo } from '@/components/atoms/Logo/Logo.atom';
+import { useUpdateDate } from '@/reducks/date/dispatches';
 
 export const Header = () => {
   const handleClick = () => {};
@@ -14,7 +15,12 @@ export const Header = () => {
         <HamburgerButton onOpen={false} />
         <UserIcon />
         <CurrentDate />
-        <Button onClick={handleClick} variant='header'>
+        <Button
+          onClick={() => {
+            useUpdateDate(new Date().toISOString());
+          }}
+          variant='header'
+        >
           今日
         </Button>
         <div className='flex gap-12'>
